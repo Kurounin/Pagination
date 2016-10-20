8821c89d84d19ae578c5204fbdfc88543e00e5a6
alan bount <alan@zeroasterisk.com>
2016-10-19 22:48:38 -0400

    updated package.js to ES6

--- package.js
+++ package.js
@@ -1,30 +1,32 @@
 Package.describe({
-    "name": "kurounin:pagination",
-    "summary": "Meteor pagination done right. Usable in ReactJS or Blaze templates.",
-    "version": "1.0.13",
-    "git": "https://github.com/Kurounin/Pagination.git"
+  name: 'kurounin:pagination',
+  summary: 'Meteor pagination done right. Usable in ReactJS or Blaze templates.',
+  version: '1.0.13',
+  git: 'https://github.com/Kurounin/Pagination.git',
+  documentation: 'README.md',
 });
 
-Package.onUse(function (api) {
-    api.versionsFrom("METEOR@1.2.1");
-    api.use([
-        "meteor-base",
-        "check",
-        "underscore",
-        "mongo",
-        "tmeasday:publish-counts@0.7.3"
-    ]);
+Package.onUse((api) => {
+  api.versionsFrom('METEOR@1.2.1');
+  api.use([
+    'ecmascript',
+    'meteor-base',
+    'check',
+    'underscore',
+    'mongo',
+    'tmeasday:publish-counts@0.7.3',
+  ]);
 
-    api.addFiles([
-        "server/pagination.js"
-    ], "server");
+  api.addFiles([
+    'server/pagination.js',
+  ], 'server');
 
-    api.use([
-        "reactive-var",
-        "reactive-dict"
-    ], "client");
+  api.use([
+    'reactive-var',
+    'reactive-dict',
+  ], 'client');
 
-    api.addFiles([
-        "client/pagination.js"
-    ], "client");
+  api.addFiles([
+    'client/pagination.js',
+  ], 'client');
 });
