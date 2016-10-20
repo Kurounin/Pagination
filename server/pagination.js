5c0564eb5118bfd39731ca04e85c0ac30898756c
alan bount <alan@zeroasterisk.com>
2016-10-19 22:48:52 -0400

    updated server to ES6

--- server/pagination.js
+++ server/pagination.js
@@ -1,99 +1,117 @@
-var Pagination = (function () {
-    function Pagination(collection, settings) {
-        if (!(this instanceof Meteor.Pagination)) {
-            throw new Meteor.Error(4000, "The Meteor.Pagination instance has to be initiated with `new`");
-        }
+import _ from 'underscore';
+import { Meteor } from 'meteor/meteor';
+import { check, Match } from 'meteor/check';
+import { Counts } from 'meteor/tmeasday:publish-counts';
 
-        settings = _.extend(
-            {
-                filters: {},
-                dynamic_filters: function () {
-                    return {};
-                }
-            },
-            settings || {}
-        );
+const PaginationFactory = ((() => {
+  class Pagination {
+    constructor(collection, settingsIn) {
+      if (!(this instanceof Meteor.Pagination)) {
+        // eslint-disable-next-line max-len
+        throw new Meteor.Error(4000, 'The Meteor.Pagination instance has to be initiated with `new`');
+      }
 
-        if (typeof settings.filters != 'object') {
-            throw new Meteor.Error(4001, "Invalid filters provided. Server side filters need to be an object!");
-        }
+      const settings = _.extend(
+        {
+          filters: {},
+          dynamic_filters() {
+            return {};
+          },
+        },
+        settingsIn || {}
+      );
 
-        if (typeof settings.dynamic_filters != 'function') {
-            throw new Meteor.Error(4002, "Invalid dynamic filters provided. Server side dynamic filters needs to be a function!");
-        }
+      if (typeof settings.filters !== 'object') {
+        // eslint-disable-next-line max-len
+        throw new Meteor.Error(4001, 'Invalid filters provided. Server side filters need to be an object!');
+      }
 
-        this.publish(collection, settings)
+      if (typeof settings.dynamic_filters !== 'function') {
+        // eslint-disable-next-line max-len
+        throw new Meteor.Error(4002, 'Invalid dynamic filters provided. Server side dynamic filters needs to be a function!');
+      }
+
+      this.publish(collection, settings);
     }
 
-    Pagination.prototype.publish = function(collection, settings) {
-        Meteor.publish(collection._name, function(query, options) {
-            check(query, Match.Optional(Object));
-            check(options, Match.Optional(Object));
+    publish(collection, settings) {
+      Meteor.publish(collection._name, function addPub(query = {}, options = {}) {
+        check(query, Match.Optional(Object));
+        check(options, Match.Optional(Object));
 
-            var self = this,
-                findQuery = {},
-                filters = [];
-            query = query || {};
-            options = options || {};
+        const self = this;
+        let findQuery = {};
+        let filters = [];
 
-            if (!_.isEmpty(query)) {
-                filters.push(query);
-            }
+        if (!_.isEmpty(query)) {
+          filters.push(query);
+        }
 
-            if (!_.isEmpty(settings.filters)) {
-                filters.push(settings.filters);
-            }
+        if (!_.isEmpty(settings.filters)) {
+          filters.push(settings.filters);
+        }
 
-            var dynamic_filters = settings.dynamic_filters.call(self);
+        const dynamic_filters = settings.dynamic_filters.call(self);
 
-            if (typeof dynamic_filters == 'object') {
-                if (!_.isEmpty(dynamic_filters)) {
-                    filters.push(dynamic_filters);
-                }
-            } else {
-                throw new Meteor.Error(4002, "Invalid dynamic filters return type. Server side dynamic filters needs to be a function that returns an object!");
-            }
+        if (typeof dynamic_filters === 'object') {
+          if (!_.isEmpty(dynamic_filters)) {
+            filters.push(dynamic_filters);
+          }
+        } else {
+          // eslint-disable-next-line max-len
+          throw new Meteor.Error(4002, 'Invalid dynamic filters return type. Server side dynamic filters needs to be a function that returns an object!');
+        }
 
-            if (typeof settings.transform_filters === "function") {
-                filters = settings.transform_filters.call(self, filters);
-            }
+        if (typeof settings.transform_filters === 'function') {
+          // eslint-disable-next-line max-len
+          filters = settings.transform_filters.call(self, filters);
+        }
 
-            if (filters.length > 0) {
-                if (filters.length > 1) {
-                    findQuery['$and'] = filters;
-                } else {
-                    findQuery = filters[0];
-                }
-            }
+        if (filters.length > 0) {
+          if (filters.length > 1) {
+            findQuery.$and = filters;
+          } else {
+            findQuery = filters[0];
+          }
+        }
 
-            Counts.publish(self, 'sub_count_' + self._subscriptionId, collection.find(findQuery), {noReady: true});
+        Counts.publish(
+          self,
+          `sub_count_${self._subscriptionId}`,
+          collection.find(findQuery),
+          { noReady: true }
+        );
 
-            var handle = collection.find( findQuery, options ).observeChanges({
-                added: function (id, fields) {
-                    var newFields = {};
+        // console.log('Pagination', collection._name, 'find',
+        // JSON.stringify(findQuery), JSON.stringify(options));
 
-                    self.added(collection._name, id, fields);
+        const handle = collection.find(findQuery, options).observeChanges({
+          added(id, fields) {
+            const newFields = {};
 
-                    newFields['sub_' + self._subscriptionId] = 1;
-                    self.changed(collection._name, id, newFields);
-                },
-                changed: function (id, fields) {
-                    self.changed(collection._name, id, fields);
-                },
-                removed: function (id) {
-                    self.removed(collection._name, id);
-                }
-            });
+            self.added(collection._name, id, fields);
 
-            self.ready();
-
-            self.onStop(function () {
-                handle.stop();
-            });
+            newFields[`sub_${self._subscriptionId}`] = 1;
+            self.changed(collection._name, id, newFields);
+          },
+          changed(id, fields) {
+            self.changed(collection._name, id, fields);
+          },
+          removed(id) {
+            self.removed(collection._name, id);
+          },
         });
-    };
 
-    return Pagination;
-})();
+        self.ready();
 
-Meteor.Pagination = Pagination;
+        self.onStop(() => {
+          handle.stop();
+        });
+      });
+    }
+  }
+
+  return Pagination;
+}))();
+
+Meteor.Pagination = PaginationFactory;
