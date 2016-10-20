2ea316856fe35b4a536e40e6b3a5e29bf3ae8676
alan bount <alan@zeroasterisk.com>
2016-10-19 22:57:11 -0400

    updated client to ES6

--- client/pagination.js
+++ client/pagination.js
@@ -1,143 +1,143 @@
-var Pagination = (function () {
-    function Pagination(collection, settings) {
-        if (!(this instanceof Meteor.Pagination)) {
-            throw new Meteor.Error(4000, "The Meteor.Pagination instance has to be initiated with `new`");
-        }
+import _ from 'underscore';
+import { Meteor } from 'meteor/meteor';
+import { ReactiveDict } from 'meteor/reactive-dict';
+import { Counts } from 'meteor/tmeasday:publish-counts';
 
-        this.collection = collection;
-        this.settings = new ReactiveDict();
-        settings = _.extend(
-            {
-                page: 1,
-                perPage: 10,
-                filters: {},
-                fields: {},
-                sort: { _id: 1 }
-            },
-            settings || {}
-        );
+const PaginationFactory = ((() => {
+  class Pagination {
+    constructor(collection, settingsIn = {}) {
+      if (!(this instanceof Meteor.Pagination)) {
+        // eslint-disable-next-line max-len
+        throw new Meteor.Error(4000, 'The Meteor.Pagination instance has to be initiated with `new`');
+      }
 
-        if (!this.currentPage()) {
-            this.currentPage(settings.page);
-        }
+      this.collection = collection;
+      this.settings = new ReactiveDict();
+      const settings = _.extend(
+        {
+          page: 1,
+          perPage: 10,
+          filters: {},
+          fields: {},
+          sort: { _id: 1 },
+        },
+        settingsIn || {}
+      );
 
-        if (!this.perPage()) {
-            this.perPage(settings.perPage);
-        }
+      if (!this.currentPage()) {
+        this.currentPage(settings.page);
+      }
 
-        if (!this.filters()) {
-            this.filters(settings.filters);
-        }
+      if (!this.perPage()) {
+        this.perPage(settings.perPage);
+      }
 
-        if (!this.fields()) {
-            this.fields(settings.fields);
-        }
+      if (!this.filters()) {
+        this.filters(settings.filters);
+      }
 
-        if (!this.sort()) {
-            this.sort(settings.sort);
-        }
+      if (!this.fields()) {
+        this.fields(settings.fields);
+      }
 
-        if (!this.ready()) {
-            this.ready(false);
-        }
+      if (!this.sort()) {
+        this.sort(settings.sort);
+      }
+
+      if (!this.ready()) {
+        this.ready(false);
+      }
     }
 
-    Pagination.prototype.currentPage = function (page) {
-        if (arguments.length === 1) {
-            if (this.settings.get("page") !== page && page >= 1) {
-                this.settings.set("page", page);
-            }
-        } else {
-            return this.settings.get("page");
+    currentPage(page) {
+      if (arguments.length === 1) {
+        if (this.settings.get('page') !== page && page >= 1) {
+          this.settings.set('page', page);
         }
-    };
+      }
+      return this.settings.get('page');
+    }
 
-    Pagination.prototype.perPage = function (perPage) {
-        if (arguments.length === 1) {
-            if (this.settings.get("perPage") !== perPage) {
-                this.settings.set("perPage", perPage);
-            }
-        } else {
-            return this.settings.get("perPage");
+    perPage(perPage) {
+      if (arguments.length === 1) {
+        if (this.settings.get('perPage') !== perPage) {
+          this.settings.set('perPage', perPage);
         }
-    };
+      }
+      return this.settings.get('perPage');
+    }
 
-    Pagination.prototype.filters = function (filters) {
-        if (arguments.length === 1) {
-            filters = !_.isEmpty(filters) ? filters : {};
-            this.settings.set("filters", filters);
-        } else {
-            return this.settings.get("filters");
+    filters(filters) {
+      if (arguments.length === 1) {
+        this.settings.set('filters', !_.isEmpty(filters) ? filters : {});
+      }
+      return this.settings.get('filters');
+    }
+
+    fields(fields) {
+      if (arguments.length === 1) {
+        this.settings.set('fields', fields);
+      }
+      return this.settings.get('fields');
+    }
+
+    sort(sort) {
+      if (arguments.length === 1) {
+        this.settings.set('sort', sort);
+      }
+      return this.settings.get('sort');
+    }
+
+    totalItems(totalItems) {
+      if (arguments.length === 1) {
+        this.settings.set('totalItems', totalItems);
+        if (this.currentPage() > 1 && totalItems <= this.perPage() * this.currentPage()) {
+          // move to last page available
+          this.currentPage(this.totalPages());
         }
-    };
+      }
+      return this.settings.get('totalItems');
+    }
 
-    Pagination.prototype.fields = function (fields) {
-        if (arguments.length === 1) {
-            this.settings.set("fields", fields);
-        } else {
-            return this.settings.get("fields");
-        }
-    };
+    totalPages() {
+      const totalPages = this.totalItems() / this.perPage();
+      return Math.ceil(totalPages || 1);
+    }
 
-    Pagination.prototype.sort = function (sort) {
-        if (arguments.length === 1) {
-            this.settings.set("sort", sort);
-        } else {
-            return this.settings.get("sort");
-        }
-    };
+    ready(ready) {
+      if (arguments.length === 1) {
+        this.settings.set('ready', ready);
+      }
+      return this.settings.get('ready');
+    }
 
-    Pagination.prototype.totalItems = function (totalItems) {
-        if (arguments.length === 1) {
-            this.settings.set("totalItems", totalItems);
+    getPage() {
+      const options = {
+        fields: this.fields(),
+        sort: this.sort(),
+        skip: (this.currentPage() - 1) * this.perPage(),
+        limit: this.perPage(),
+      };
 
-            if (this.currentPage() > 1 && totalItems <= this.perPage() * this.currentPage()) {
-                // move to last page available
-                this.currentPage(this.totalPages());
-            }
-        } else {
-            return this.settings.get("totalItems");
-        }
-    };
+      const handle = Meteor.subscribe(
+        this.collection._name,
+        this.filters(),
+        options
+      );
 
-    Pagination.prototype.totalPages = function () {
-        var totalPages = this.totalItems() / this.perPage();
+      const query = {};
 
-        return Math.ceil(totalPages ? totalPages : 1);
-    };
+      this.ready(handle.ready());
+      if (handle.ready()) {
+        this.totalItems(Counts.get(`sub_count_${handle.subscriptionId}`));
+      }
 
-    Pagination.prototype.ready = function (ready) {
-        if (arguments.length === 1) {
-            this.settings.set("ready", ready);
-        } else {
-            return this.settings.get("ready");
-        }
-    };
+      query[`sub_${handle.subscriptionId}`] = 1;
+      return this.collection.find(query, { fields: this.fields(), sort: this.sort() }).fetch();
+    }
+  }
 
-    Pagination.prototype.getPage = function() {
-        var options = {
-                fields: this.fields(),
-                sort: this.sort(),
-                skip: (this.currentPage() - 1) * this.perPage(),
-                limit: this.perPage()
-            },
-            handle = Meteor.subscribe(
-                this.collection._name,
-                this.filters(),
-                options
-            ),
-            query = {};
+  return Pagination;
+}))();
 
-        this.ready(handle.ready());
-        if (handle.ready()) {
-            this.totalItems(Counts.get('sub_count_' + handle.subscriptionId));
-        }
-
-        query['sub_' + handle.subscriptionId] = 1;
-        return this.collection.find(query, {fields: this.fields(), sort: this.sort()}).fetch();
-    };
-
-    return Pagination;
-})();
-
-Meteor.Pagination = Pagination;
+Meteor.Pagination = PaginationFactory;
